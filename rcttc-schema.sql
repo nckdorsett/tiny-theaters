1dfae428ea6e6062cc1a6d9a7d8522a925e86fc6
Nicholas Dorsett <ndorsett@dev-10.com>
2020-10-17 19:40:33 -0500

    updating work

--- rcttc-schema.sql
+++ rcttc-schema.sql
@@ -4,55 +4,47 @@ use tiny_theaters;
 
 create table excel_data (
 	id int primary key auto_increment,
-    customer_first varchar(75) not null,
-    customer_last varchar(75) not null,
-    customer_email varchar(75) not null,
-    customer_phone varchar(25) not null,
-    customer_address varchar(75) not null,
-    seat varchar(7) not null,
-    `show` varchar(75) not null,
+    customer_first varchar(100) not null,
+    customer_last varchar(100) not null,
+    customer_email varchar(100) not null,
+    customer_phone varchar(100) not null,
+    customer_address varchar(100) not null,
+    seat varchar(100) not null,
+    `show` varchar(100) not null,
     ticket_price decimal(4,2) not null,
     `date` date not null,
-    theater varchar(75) not null,
-    theater_address varchar(75) not null,
-    theater_phone varchar(25) not null,
-    theater_email varchar(75)
-
-);
-
-
-create table seat (
-	seat_id int primary key auto_increment,
-    seat_name varchar(5) not null
+    theater varchar(100) not null,
+    theater_address varchar(100) not null,
+    theater_phone varchar(100) not null,
+    theater_email varchar(100) not null
 );
 
 create table address (
 	address_id int primary key auto_increment,
-    home_address varchar(45) not null
+    home_address varchar(100) not null
 );
 
 create table email (
 	email_id int primary key auto_increment,
-    email_address varchar(75) not null
+    email_address varchar(100) not null
 );
 
 create table phone (
 	phone_id int primary key auto_increment,
-    phone_number varchar(15) not null
+    phone_number varchar(100) not null
 );
 
 create table performance (
 	performance_id int primary key auto_increment,
-    performance_title varchar(45) not null
+    performance_title varchar(100) not null
 );
 
 create table theater (
 	theater_id int primary key auto_increment,
-    `name` varchar(45) not null,
+    `name` varchar(100) not null,
     address_id int not null,
     phone_id int not null,
     email_id int not null,
-    seat_id int not null,
     constraint fk_theater_address_id
 		foreign key (address_id)
         references address(address_id),
@@ -62,17 +54,23 @@ create table theater (
 	constraint fk_theater_phone_id
 		foreign key (phone_id)
         references phone(phone_id),
-	constraint fk_theater_seat_id
-		foreign key (seat_id)
-        references seat(seat_id),
 	constraint uq_name_email
 		unique (`name`, email_id)
 );
 
+create table seat (
+	seat_id int primary key auto_increment,
+    theater_id int not null,
+    seat_name varchar(100) not null,
+    constraint fk_seat_theater_id
+		foreign key (theater_id)
+        references theater(theater_id)
+);
+
 create table customer (
 	customer_id int primary key auto_increment,
-    first_name varchar(45) not null,
-    last_name varchar(45) not null,
+    first_name varchar(100) not null,
+    last_name varchar(100) not null,
     email_id int not null,
     phone_id int null,
     address_id int null,
@@ -122,6 +120,9 @@ create table theater_performance (
 	constraint uq_theater_performance_customer_seat
 		unique (theater_performance_id, customer_id, seat_id)
 );
+
+
+
 
 
 
